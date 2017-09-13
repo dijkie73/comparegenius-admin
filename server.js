741bf2841ca2f5605c38b66f15bbd8926cd0a4b9
dijkie73 <dijkie@gmail.com>
2017-09-13 02:26:09 +0200

    new try

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
+const ENV = require(path.join(__dirname + '/env/' + (process.env.NODE_ENV ? process.env.NODE_ENV : 'dev') + '.json'));
 const forceSSL = function () {
     return function (req, res, next) {
         if (req.headers['x-forwarded-proto'] !== 'https') {
